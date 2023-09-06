880f0b20f5c1a35df2c1d6d07e2e2622082756b3
Parker <admin@parkerhindman.com>
2023-09-05 20:27:08 -0400

    Update nautilus.sh

--- install-scripts/nautilus.sh
+++ install-scripts/nautilus.sh
@@ -4,9 +4,9 @@ rm -rf ../.git
 rm ../README.md
 
 # po
-sed -i 's/folder_i18n/folder-color/' ../po/POTFILES.in
+sed -i 's/folder_i18n/nautilus-emblems/' ../po/POTFILES.in
 sed -i 's/folder_path/nautilus-extension/' ../po/POTFILES.in
-sed -i 's/folder_i18n/folder-color/' ../nautilus-extension/folder-color.py
+sed -i 's/folder_i18n/nautilus-emblems/' ../nautilus-extension/folder-color.py
 
 # myself
 rm -rf ../install-scripts
