8c4b14e28bfd831151889141eb00dce474f0775d
Parker <admin@parkerhindman.com>
2023-09-05 22:58:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ sed -i 's/folder_i18n/nautilus-emblems/' ./po/POTFILES.in
 sed -i 's/folder_path/nautilus-extension/' ./po/POTFILES.in
 sed -i 's/folder_i18n/nautilus-emblems/' ./nautilus-extension/nautilus-emblems.py
 
+
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 cp ./nautilus-extension/nautilus-emblems.py /usr/share/nautilus-python/extensions/
 
 echo "Done"
