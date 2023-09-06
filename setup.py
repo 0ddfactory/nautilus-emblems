0078a0f14701f16d4b6f40078f5f845f436d1c92
Parker <admin@parkerhindman.com>
2023-09-05 22:48:05 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+import os, sys, glob, DistUtilsExtra.auto
+
+# Create data files
+data = [ ('/usr/share/nautilus-python/extensions',   ['nautilus-extension/folder-color.py']),
+         ('/usr/share/icons/hicolor/16x16/actions',  glob.glob('icons/hicolor/16x16/actions/*.svg')),
+         ('/usr/share/icons/Yaru/16x16/places',      glob.glob('icons/Yaru/16x16/places/*.svg')),
+         ('/usr/share/icons/Yaru/16x16@2x/places',   glob.glob('icons/Yaru/16x16@2x/places/*.svg')),
+         ('/usr/share/icons/Yaru/24x24/places',      glob.glob('icons/Yaru/24x24/places/*.svg')),
+         ('/usr/share/icons/Yaru/24x24@2x/places',   glob.glob('icons/Yaru/24x24@2x/places/*.svg')),
+         ('/usr/share/icons/Yaru/32x32/places',      glob.glob('icons/Yaru/32x32/places/*.svg')),
+         ('/usr/share/icons/Yaru/32x32@2x/places',   glob.glob('icons/Yaru/32x32@2x/places/*.svg')),
+         ('/usr/share/icons/Yaru/48x48/places',      glob.glob('icons/Yaru/48x48/places/*.svg')),
+         ('/usr/share/icons/Yaru/48x48@2x/places',   glob.glob('icons/Yaru/48x48@2x/places/*.svg')),
+         ('/usr/share/icons/Yaru/256x256/places',    glob.glob('icons/Yaru/256x256/places/*.svg')),
+         ('/usr/share/icons/Yaru/256x256@2x/places', glob.glob('icons/Yaru/256x256@2x/places/*.svg')) ]
+
+# Setup stage
+DistUtilsExtra.auto.setup(
+    name         = "folder-color",
+    version      = "0.3.2",
+    description  = "Change your folder color with just a click",
+    author       = "Marcos Alvarez Costales",
+    author_email = "marcos.costales@gmail.com",
+    url          = "https://github.com/costales/folder-color",
+    license      = "GPL3",
+    data_files   = data
+)
+
