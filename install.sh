98c57813c2f7e603b2690cf9c8f75a366aea06b5
Parker <admin@parkerhindman.com>
2023-09-05 21:00:50 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/bash]
 
-rm -rf ./.git
-rm ./README.md
+#Install Script
 
 # po
 sed -i 's/folder_i18n/nautilus-emblems/' ./po/POTFILES.in
@@ -9,8 +8,5 @@ sed -i 's/folder_path/nautilus-extension/' ./po/POTFILES.in
 sed -i 's/folder_i18n/nautilus-emblems/' ./nautilus-extension/nautilus-emblems.py
 
 cp ./nautilus-extension/nautilus-emblems.py /usr/share/nautilus-python/extensions/
-
-# myself
-rm -rf ./install-scripts
 
 echo "Done"
