27ca1572d08a9e05985100fcc2f47a757ee06c99
Parker <admin@parkerhindman.com>
2023-09-05 20:55:18 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sed -i 's/folder_i18n/nautilus-emblems/' ./po/POTFILES.in
 sed -i 's/folder_path/nautilus-extension/' ./po/POTFILES.in
 sed -i 's/folder_i18n/nautilus-emblems/' ./nautilus-extension/nautilus-emblems.py
 
-cp ./nautilus-extension/nautilus-emblems.py /usr/share/share/nautilus-python/extensions/
+cp ./nautilus-extension/nautilus-emblems.py /usr/share/nautilus-python/extensions/
 
 # myself
 rm -rf ./install-scripts
