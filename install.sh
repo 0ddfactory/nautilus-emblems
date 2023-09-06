23a4dfcf0b99bc05ce0ad34a73d40b78dc489e18
Parker <admin@parkerhindman.com>
2023-09-05 20:46:26 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf ./.git
+rm ./README.md
+
+# po
+sed -i 's/folder_i18n/nautilus-emblems/' ./po/POTFILES.in
+sed -i 's/folder_path/nautilus-extension/' ./po/POTFILES.in
+sed -i 's/folder_i18n/nautilus-emblems/' ./nautilus-extension/nautilus-emblems.py
+
+cp ./nautilus-extension/nautilus-emblems.py /usr/share/share/nautilus-python/extensions/
+
+# myself
+rm -rf ./install-scripts
+
+echo "Done"
